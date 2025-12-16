d1fa3f975b668163f3c48f6c0065a02d6ed4a065
Ella Murphy <ella.murphy@student.cgcc.edu>
2025-12-16 04:22:59 -0800

    Add queue configuration to config.php

--- config.php
+++ config.php
@@ -56,6 +56,9 @@ return [
         'cookie' => 'flarum_session',
         'lifetime' => 60,
     ],
+    'queue' => [
+        'driver' => 'sync',
+    ],
     'url' => $appUrl ?: 'http://localhost',
     'paths' => [
         'api' => 'api',
